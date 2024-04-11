974ff85d443f2c12d7b0023e130d6b60284ce3ef
Ayush Das <dasayush953@gmail.com>
2024-04-11 15:02:07 +0600

    First Push
    Pushing all file

---
+++ src/mainpkg/FXMLLoginSceneController.java
@@ -0,0 +1,102 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package mainpkg;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author dasay
+ */
+public class FXMLLoginSceneController implements Initializable {
+
+    @FXML
+    private TextField idTextField;
+    @FXML
+    private PasswordField passwordTextField;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void loginButtonOnAction(ActionEvent event) throws IOException {
+                //use BufferedReader OR Scanner
+        File file = new File("registerText.txt");
+        Scanner sc; String str=null;
+        try {
+            sc = new Scanner(file);
+            //outputTxtArea.setText(null);
+            while(sc.hasNextLine()){
+                str=sc.nextLine();
+                String[] tokens;
+                tokens = str.split(",");
+                //str = "Id="+tokens[0]+", Name="+tokens[1]
+                //        +", Cgpa="+tokens[2];
+                //outputTxtArea.appendText(str+"\n");
+                //outputTxtArea.appendText("\n");
+                
+                if((idTextField.getText().equals(tokens[0]))&&(passwordTextField.getText().equals(tokens[1]))){
+                    System.out.println("Hello "+tokens[2]+" "+tokens[3]);
+                    
+                    if("Nurse".equals(tokens[2])){
+                            Parent mainSceneParent = FXMLLoader.load(getClass().getResource("FXMLMainScene.fxml"));
+        
+                            Scene scene = new Scene(mainSceneParent);
+
+                            Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+                            window.setScene(scene);
+                            window.show();
+                    }
+                
+                
+                }
+                
+                
+                
+                
+                
+            }
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(FXMLLoginSceneController.class.getName()).log(Level.SEVERE, null, ex);
+        } 
+
+    }
+
+    @FXML
+    private void singupLableOnMouseClick(MouseEvent event) throws IOException {
+        Parent mainSceneParent = FXMLLoader.load(getClass().getResource("FXMLSingupScene.fxml"));
+        
+        Scene scene = new Scene(mainSceneParent);
+        
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(scene);
+        window.show();
+    }
+    
+}
---
+++ src/mainpkg/FXMLMainSceneController.java
@@ -0,0 +1,86 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package mainpkg;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+
+/**
+ * FXML Controller class
+ *
+ * @author dasay
+ */
+public class FXMLMainSceneController implements Initializable {
+
+    
+    @FXML
+    private BorderPane mainBorderPane;
+    @FXML
+    private AnchorPane welcomeAnchorePane;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+        mainBorderPane.setCenter(welcomeAnchorePane);
+        
+    }    
+
+    @FXML
+    private void profileButtonOnAction(ActionEvent event) {
+        loadPage("ProfileFXML");
+        
+    }
+
+    @FXML
+    private void findPatientsOnAction(ActionEvent event) {
+        loadPage("FindPatientFXML");
+    }
+
+    @FXML
+    private void schedulesButtonOnAction(ActionEvent event) {
+        loadPage("SchedulesFXML");
+    }
+
+    @FXML
+    private void assignedPatientButtonOnAction(ActionEvent event) {
+        loadPage("AssignedPatientsFXML");
+    }
+
+    @FXML
+    private void trainingButtonOnAction(ActionEvent event) {
+        loadPage("Training & CertificationsFXML");
+    }
+
+    @FXML
+    private void exitButtonOnAction(ActionEvent event) {
+        System.exit(0);
+    }
+    
+    private void loadPage(String page){
+        Parent root = null;
+        try{
+          root = FXMLLoader.load(getClass().getResource(page+".fxml"));
+        }catch(IOException ex){
+              Logger.getLogger(FXMLMainSceneController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        mainBorderPane.setCenter(root);      
+    }
+
+ 
+    
+}
